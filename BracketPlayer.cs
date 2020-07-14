f2226b69e0be502530da970d728e4508b4e00370
Lunaris <aerik0712@gmail.com>
2020-07-14 15:47:54 -0700

    Added the player class

--- BracketPlayer.cs
+++ BracketPlayer.cs
@@ -6,8 +6,31 @@ namespace BracketsBrackets
 {
     class BracketPlayer
     {
-        public string Name;
+        public string name;
         public int[] games;
 
+        public BracketPlayer(string name, int[] games)
+        {
+            this.name = name;
+            this.games = games;
+        }
+
+        public override bool Equals(object obj)
+        { 
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                BracketPlayer p = (BracketPlayer)obj;
+                return (name == p.name) && (games == p.games);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(name, games);
+        }
     }
 }
