7b41a4ffc7e5f28c01cd3c7cde1f093fd59d96d0
Lunaris <aerik0712@gmail.com>
2020-07-14 13:50:26 -0700

    Add project files.

---
+++ BracketPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BracketsBrackets
+{
+    class BracketPlayer
+    {
+        public string Name;
+        public int[] games;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BracketsBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
