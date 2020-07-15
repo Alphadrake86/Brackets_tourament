f43f7b26f0fb7b5025e3a00e751e6f7c78f20cf1
Alphadrake86 <anderson8221@student.cptc.edu>
2020-07-15 13:04:47 -0700

    Probably added the ability to get the winner from each bracket

--- Bracket.cs
+++ Bracket.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,7 +9,7 @@ namespace BracketsBrackets
     {
         public static Random random;
 
-        private BracketGame[] Games;
+        private ArrayList Games;
 
         public int NumOfPlayers { get; private set; }
         public int MaxPlayers { get; set; }
@@ -18,6 +19,12 @@ namespace BracketsBrackets
             random = new Random();
         }
 
+        public Bracket(int max)
+        {
+            MaxPlayers = max;
+        }
+
+
         public bool IsFull()
         {
             return NumOfPlayers < MaxPlayers;
@@ -25,11 +32,32 @@ namespace BracketsBrackets
 
         public void AddGame(BracketGame game)
         {
-            Games[NumOfPlayers] = game;
+            Games.Add(game);
             NumOfPlayers++;
         }
 
+        public BracketGame GetWinners()
+        {
+            return GetWinners( new ArrayList(Games), 1);
+        }
 
-        
+        private BracketGame GetWinners(ArrayList gamesTemp, int gameNum)
+        {
+            if (gamesTemp.Count == 1) return (BracketGame)gamesTemp[0];
+            else
+            {
+                ArrayList playersTemp = new ArrayList();
+                foreach (BracketGame game in gamesTemp)
+                {
+                    playersTemp.Add(game.GetWinner(gameNum));
+                }
+                gamesTemp.Clear();
+                for (int i = 0; i < playersTemp.Count; i+=2)
+                {
+                    gamesTemp.Add(new BracketGame((BracketPlayer)playersTemp[i], (BracketPlayer)playersTemp[i + 1]));
+                }
+                return GetWinners(gamesTemp, gameNum++);
+            }
+        }
     }
 }
