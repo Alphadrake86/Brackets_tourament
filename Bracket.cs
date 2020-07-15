1d5a5534105b7d93ada0ab72010675a230a50634
Alphadrake86 <anderson8221@student.cptc.edu>
2020-07-15 11:41:55 -0700

    testing username dealio

---
+++ Bracket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BracketsBrackets
+{
+    class Bracket
+    {
+        public static Random random;
+
+        private BracketGame[] Games;
+
+        public int NumOfPlayers { get; private set; }
+        public int MaxPlayers { get; set; }
+
+        static Bracket()
+        {
+            random = new Random();
+        }
+
+        public bool IsFull()
+        {
+            return NumOfPlayers < MaxPlayers;
+        }
+
+        public void AddGame(BracketGame game)
+        {
+            Games[NumOfPlayers] = game;
+            NumOfPlayers++;
+        }
+
+
+        
+    }
+}
